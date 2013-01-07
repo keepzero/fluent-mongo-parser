7045222aaf3c553f7bc73eb1a32f17c4d3fc1f24
KeepZero <i@keepzero.net>
2013-01-07 10:16:17 +0800

    Update log.py add set_log_collection function

--- log.py
+++ log.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 class LogDocGenerator:
-    def __init__(self, log_source):
+    def __init__(self, log_source=None):
         """docstring for __init__"""
-        self.collection = log_source
+        self.collection = log_source if log_source else None
 
     def get_log_docs(self, condition={}):
         """docstring for get_log_docs"""
@@ -11,3 +11,6 @@ class LogDocGenerator:
             return self.collection.find()
         else:
             return self.collection.find(condition)
+
+    def set_log_source(self, log_source):
+        self.collection = log_source
