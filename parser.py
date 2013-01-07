1d136691fad6a3b91d2f08f0bb83fb75e1f874d9
KeepZero <i@keepzero.net>
2013-01-07 10:16:35 +0800

    Add usage guide

--- parser.py
+++ parser.py
@@ -5,20 +5,27 @@ from manager import PluginManager
 from log import LogDocGenerator
 
 def main():
+
+    # 1. load all plugins
     plugin_manager = PluginManager()
 
-    #keywords = ['counter', 'ip']
-    #keywords = ['counter']
-    keywords = ['ip']
-
+    # 2. get one or more mongodb collection 
     ms = MongoSource()
     collection = ms.get_collection("net-test", "ename_access")
+
+    # 3. make a log_generator
     log_generator = LogDocGenerator(collection)
 
+    # 4. use condition to get filtered logs
     #condition = {"host":"192.168.1.57"}
     condition = {}
+
+    # 5. use keywords plugins to parse logs
+    keywords = ['ip']
     for log_doc in log_generator.get_log_docs(condition):
         plugin_manager.call_method('process', args=log_doc, keywords=keywords)
+
+    # 6. give a report
     plugin_manager.call_method('report', args={}, keywords=keywords)
 
 if __name__ == '__main__':
