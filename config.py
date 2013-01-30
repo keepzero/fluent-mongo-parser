68a6d0e386ca86feaf8018e814ef84e741027ce6
KeepZero <i@keepzero.net>
2013-01-30 10:17:33 +0800

    Add authenticate to config.py

--- config.py
+++ config.py
@@ -3,22 +3,23 @@
 from pymongo import MongoClient
 
 CONFIG = {
-    'host':  '192.168.1.211',
+    'host':  '192.168.10.202',
     'port':  27017,
-    'user':  'mongo',
-    'pswd':  'mongo',
+    'user':  'root',
+    'pswd':  'password',
 }
 
 def get_connection():
-    """Simple method to get mongo connection"""
+    """
+    Simple method to get mongo connection.
+       You may need to auth with mongo.
+    """
     return MongoClient(CONFIG['host'], CONFIG['port'])
 
 class MongoSource:
-    def __init__(self, host=None, port=27017, user="mongo", pswd="mongo"):
-        if host:
-            self.connection = MongoClient(host, port)
-        else:
-            self.connection = MongoClient(CONFIG['host'], CONFIG['port'])
+    def __init__(self, host="127.0.0.1", port=27017, user="mongo", pswd="mongo"):
+        self.connection = MongoClient(host, port)
+        self.connection["admin"].authenticate(user, pswd)
 
     def get_db(self, dbname):
         """Get a mongo Database"""
