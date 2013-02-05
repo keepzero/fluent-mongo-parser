a71a7e1a5c59d5ffa8fafa06c4bd176e37c24eb0
KeepZero <i@keepzero.net>
2013-02-05 14:01:55 +0800

    fix tab and space

--- plugins/plugin_nginx_error.py
+++ plugins/plugin_nginx_error.py
@@ -14,7 +14,7 @@ class NginxError(Plugin):
 
     def process(self, **kwargs):
         """docstring for process"""
-        self.total_line += 1
+        #self.total_line += 1
         self.level_dict[kwargs['level']] += 1
         message = kwargs['message']
         m = re.match(".*client: (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*", message)
@@ -26,9 +26,11 @@ class NginxError(Plugin):
 
     def report(self, **kwargs):
         """docstring for report"""
-        print "== Nginx Error =="
-        print "Nginx total error line: %d" % self.total_line
+        print "====== Nginx Error ======"
+        #print "Nginx total error line: %d" % self.total_line
         for level in self.level_dict.keys():
             print "%s: %d" % (level, self.level_dict[level])
-        for client in self.client_dict.keys():
-            print "%s: %d" % (client, self.client_dict[client])
+        clients = sorted(self.client_dict.items(), lambda x, y: cmp(x[1], y[1]), reverse = True)
+        for client in clients:
+            if client[1] > 50:
+                print "%s: %d" % (client[0], client[1])
